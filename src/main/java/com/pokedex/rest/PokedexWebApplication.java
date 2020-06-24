3782b0f8613fee1924bc21541dbe7ff9f0ca513b
Carlos <cramirezc612@gmail.com>
2020-06-23 20:27:11 -0400

    CR - Se agrega clase main y pom

---
+++ src/main/java/com/pokedex/rest/PokedexWebApplication.java
@@ -0,0 +1,15 @@
+package com.pokedex.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class PokedexWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pokedex/rest/PokedexWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokedex.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
